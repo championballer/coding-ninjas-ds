c340b02a68dcce9861b1834dac1dc3c0faccbbe4
Shiv Kumar <championballer08@gmail.com>
2019-05-17 22:42:35 +0530

    Added undo in stack

---
+++ Stack/undo.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include"LL Implementation.h"
+
+using namespace std;
+
+int main()
+{
+	Stack<char> myStack;
+	Stack<int>` 
+	char choice;
+	do{
+		int a;
+		int b;
+		cin>>a>>choice>>b;
+		myStack.push(choice);
+
+	}while(choice!='n')
+}
